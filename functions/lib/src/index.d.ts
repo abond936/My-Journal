076822f31e1852f454e3449207c85c0c51cdeee8
Alan B <alan.bond8@yahoo.com>
2025-06-21 06:41:50 -0500

    Stop tracking files in functions directory

--- functions/lib/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as functions from "firebase-functions";
-export declare const scheduledAggregateCardStats: functions.CloudFunction<unknown>;
-export declare const onDemandAggregateCardStats: functions.HttpsFunction & functions.Runnable<any>;
